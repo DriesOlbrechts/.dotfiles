8c097749960fa98c829439402acbe46787156e69
Dries1234 <driesolbrechts@gmail.com>
2022-07-30 23:38:37 +0200

    change init file

--- neovim/.config/nvim/lua/dodo/init.lua
+++ neovim/.config/nvim/lua/dodo/init.lua
@@ -1,15 +1,2 @@
 require("dodo.settings")
-require("dodo.keymaps")
 require("dodo.plugins")
-
-require("dodo.telescope")
-
-require("dodo.treesitter")
-require("dodo.lsp")
-require("dodo.cmp")
-
-require("dodo.lualine")
-require("dodo.colorizer")
-require("dodo.markdown")
-require("dodo.discord")
-require("dodo.neotree")
