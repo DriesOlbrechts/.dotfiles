d998aaa38dc4bddeeb935cd8211a892de9075dfe
Dries1234 <driesolbrechts@gmail.com>
2022-10-18 01:06:53 +0200

    This is a basic implementation that works.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    
+    # Set variables
     set fish_greeting
     set EDITOR nvim
+    
+    # Terminal welcome
+    neofetch
 
 
 
