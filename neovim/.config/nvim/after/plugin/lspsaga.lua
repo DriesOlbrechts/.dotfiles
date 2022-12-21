0084d8088b7afbbb9047fc2ae3e8042a28d382b9
Dries1234 <driesolbrechts@gmail.com>
2022-12-21 16:12:22 +0100

    This really should not take 19 minutes to build.

--- neovim/.config/nvim/after/plugin/lspsaga.lua
+++ neovim/.config/nvim/after/plugin/lspsaga.lua
@@ -4,5 +4,5 @@ local saga = require('lspsaga')
 saga.init_lsp_saga()
 
 keymap("n", "<leader>pd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
-keymap("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", { silent = true })
+keymap("n", "<leader>o", "<cmd>Lspsaga outline<CR>", { silent = true })
 keymap("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", { silent = true })
