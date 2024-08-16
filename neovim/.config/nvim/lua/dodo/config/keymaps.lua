f966f65082ba361f2db2387976a097b277700bfd
DriesOlbrechts <driesolbrechts@gmail.com>
2024-08-16 13:34:21 +0200

    Never Run This Commit As Root

--- neovim/.config/nvim/lua/dodo/config/keymaps.lua
+++ neovim/.config/nvim/lua/dodo/config/keymaps.lua
@@ -3,7 +3,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 
-vim.keymap.set({'v'}, '<C-C>', [["+y]], {
-    desc = 'Copy to system clipboard',
-    noremap = true, silent = true
+vim.keymap.set({ 'v' }, '<C-C>', [["+y]], {
+	desc = 'Copy to system clipboard',
+	noremap = true,
+	silent = true
 })
--- neovim/.config/nvim/lua/dodo/config/lazy.lua
+++ neovim/.config/nvim/lua/dodo/config/lazy.lua
@@ -1,49 +1,49 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-        vim.fn.system({
-                "git",
-                "clone",
-                "--filter=blob:none",
-                "https://github.com/folke/lazy.nvim.git",
-                "--branch=stable", -- latest stable release
-                lazypath,
-        })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 
-        vim.notify("Installed lazy.")
+	vim.notify("Installed lazy.")
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-        defaults = {
-                lazy = true,
-        },
-        spec = {
-                {import = "dodo.plugins"},
-        },
-        install = {
-                missing = true,
-                colorscheme = { "catppuccin" }
-        },
-        performance = {
-                cache = {
-                        enabled = true,
-                },
-                rtp = {
-                        disabled_plugins = {
-                                "gzip",
-                                "tar",
-                                "tarPlugin",
-                                "zip",
-                                "zipPlugin",
-                                "tutor_mode_plugin",
-                                "tutor",
-                                "vimball",
-                                "vimballPlugin",
-                                "bugreport",
-                                "fzf",
-                        },
-                },
-        },
+	defaults = {
+		lazy = true,
+	},
+	spec = {
+		{ import = "dodo.plugins" },
+	},
+	install = {
+		missing = true,
+		colorscheme = { "catppuccin" }
+	},
+	performance = {
+		cache = {
+			enabled = true,
+		},
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"tar",
+				"tarPlugin",
+				"zip",
+				"zipPlugin",
+				"tutor_mode_plugin",
+				"tutor",
+				"vimball",
+				"vimballPlugin",
+				"bugreport",
+				"fzf",
+			},
+		},
+	},
 })
