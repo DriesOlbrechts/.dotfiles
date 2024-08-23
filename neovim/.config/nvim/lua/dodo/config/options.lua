1c36731d29a5a3b07c8c7a8923667c0bb98c256f
DriesOlbrechts <driesolbrechts@gmail.com>
2024-08-23 13:14:24 +0200

    fixed conflicts (LOL merge -s ours; push -f)

--- neovim/.config/nvim/lua/dodo/config/options.lua
+++ neovim/.config/nvim/lua/dodo/config/options.lua
@@ -1,6 +1,6 @@
 local opt = vim.opt
 
-vim.cmd.colorscheme('catppuccin')
+vim.cmd.colorscheme("catppuccin")
 
 opt.termguicolors = true
 opt.compatible = false
@@ -8,27 +8,27 @@ opt.compatible = false
 opt.confirm = true
 
 opt.undofile = true
-opt.undodir = os.getenv("HOME") .. '/.config/nvim/undo'
+opt.undodir = os.getenv("HOME") .. "/.config/nvim/undo"
 opt.undolevels = 10000
 
 opt.errorbells = false
 
 opt.title = true
-opt.titlestring = '%<%F%=%l/%L-%P'
+opt.titlestring = "%<%F%=%l/%L-%P"
 
 opt.swapfile = false
-opt.mouse = 'a'
+opt.mouse = "a"
 opt.updatetime = 69
-opt.completeopt = 'menu,menuone,preview,noinsert,noselect'
+opt.completeopt = "menu,menuone,preview,noinsert,noselect"
 opt.ignorecase = true
 opt.smartcase = true
 opt.cursorline = true
-opt.guicursor = 'a:block,i:ver20,v-r:hor20'
+opt.guicursor = "a:block,i:ver20,v-r:hor20"
 opt.hlsearch = false
 opt.list = true
 opt.listchars = {
-	tab = '| ',
-	trail = '.'
+	tab = "| ",
+	trail = ".",
 }
 opt.number = true
 opt.relativenumber = true
@@ -37,9 +37,8 @@ opt.scrolloff = 8
 opt.laststatus = 3
 opt.showmode = false
 
-
-opt.colorcolumn = '100'
-opt.signcolumn = 'yes'
+opt.colorcolumn = "100"
+opt.signcolumn = "yes"
 opt.splitbelow = true
 opt.splitright = true
 opt.autoindent = true
@@ -50,8 +49,15 @@ opt.tabstop = 4
 opt.shiftwidth = 4
 opt.textwidth = 100
 opt.wrap = false
-opt.formatoptions = 'crqn2lj'
+opt.formatoptions = "crqn2lj"
 opt.tildeop = true
 
+-- Folding
+opt.foldenable = false
+opt.foldlevel = 999
+
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.smoothscroll = true
 
 vim.g.netrw_liststyle = 1
