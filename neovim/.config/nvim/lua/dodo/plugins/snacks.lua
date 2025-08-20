31e9f81f6f8d655ab345c069fdd4e0a1aaf99bb0
DriesOlbrechts <driesolbrechts@gmail.com>
2025-08-21 00:14:31 +0200

    another color scheme switch

--- neovim/.config/nvim/lua/dodo/plugins/snacks.lua
+++ neovim/.config/nvim/lua/dodo/plugins/snacks.lua
@@ -52,5 +52,14 @@ return {
 				},
 			},
 		},
+		init = function()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "VeryLazy",
+				callback = function()
+					vim.api.nvim_set_hl(0, "SnacksPicker", { bg = "none", nocombine = true })
+					vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = "none", bg = "none", nocombine = true })
+				end,
+			})
+		end,
 	},
 }
