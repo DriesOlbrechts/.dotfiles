01143dd8af3dac7254e10447d126f6479467c206
DriesOlbrechts <driesolbrechts@gmail.com>
2026-08-26 23:23:07 +0200

    jobs... steve jobs

--- neovim/.config/nvim/lua/dodo/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/dodo/plugins/treesitter.lua
@@ -5,20 +5,6 @@ return {
 		config = function()
 			require("treesitter-context").setup()
 
-			require("nvim-treesitter.configs").setup({
-				auto_install = true,
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-				indent = {
-					enable = true,
-				},
-				context_commentstring = {
-					enable = true,
-					enable_autocmd = false,
-				},
-			})
 		end,
 		build = ":TSUpdate",
 		dependencies = {
